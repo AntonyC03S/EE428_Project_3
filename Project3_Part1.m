3b30875edc9a703516460a1271f0e07ea5457c36
Antony Chen <122947939+AntonyC03S@users.noreply.github.com>
2026-02-05 00:12:05 -0800

    Update Project3_Part1.m

--- Project3_Part1.m
+++ Project3_Part1.m
@@ -3,19 +3,56 @@ clear;
 RGB_Go_Board = imread('Images/Go_Board.png');
 Gray_Go_Board  = im2gray(RGB_Go_Board);
 
-x= 178;
-y= 145;
-Gray_Black_Piece = RGB_Go_Board(y-15:y+15,x-18:x+18);
 
-% x= 744;
-% y= 137;
-% Gray_White_Piece = RGB_Go_Board(y-12:y+10,x-15:x+15);
+% threshold = 200;
+% mask = Gray_Go_Board(:,:,1) > threshold;
+% newRgbImage = Gray_Go_Board;
+% R = newRgbImage(:,:,1);
+% R(mask) = 255;
+% finalImage = cat(3, R);
+% Gray_Go_Board = finalImage;
+% 
+% x= 178;
+% y= 145;
+% Gray_Black_Piece = Gray_Go_Board(y-15:y+15,x-18:x+18);
+% 
+% 
 x= 225;
 y= 65;
-Gray_White_Piece = RGB_Go_Board(y-8:y+8,x-12:x+12);
+Gray_White_Piece = Gray_Go_Board(y-8:y+8,x-14:x+12);
+% 
+% 
+% A = normxcorr2(Gray_Black_Piece,Gray_Go_Board);
+% B = normxcorr2(Gray_White_Piece,Gray_Go_Board);
 
+
+%-------------------
+x= 178;
+y= 145;
+Gray_Black_Piece = Gray_Go_Board(y-15:y+15,x-18:x+18);
 A = normxcorr2(Gray_Black_Piece,Gray_Go_Board);
-B = normxcorr2(Gray_White_Piece,Gray_Go_Board);
+
+
+
+threshold = 50;
+mask = Gray_Go_Board(:,:,1) < threshold;
+newRgbImage = Gray_Go_Board;
+R = newRgbImage(:,:,1);
+R(mask) = 150;
+finalImage = cat(3, R);
+Gray_Go_Board = finalImage;
+
+threshold = 220;
+mask = Gray_Go_Board(:,:,1) > threshold;
+newRgbImage = Gray_Go_Board;
+R = newRgbImage(:,:,1);
+R(mask) = 0;
+finalImage = cat(3, R);
+Gray_Go_Board = finalImage;
+
+B = normxcorr2(Gray_Black_Piece,Gray_Go_Board);
+%-------------
+
 tiledlayout(2, 3, 'TileSpacing', 'compact', 'Padding', 'compact');
 nexttile;
 imshow(Gray_Go_Board)
